a3ceb665b44e0ffe7f2fe5f9b084ac495208d3b4
Abdulbasit Imam <abdulbasitkayode@gmail.com>
2024-12-18 02:36:28 +0100

    Typed Pill component with title prop

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,24 +1,10 @@
-// const Pill: React.FC = () => {
-//     return (
-//       <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
-//         <p className=" text-sm ">Title</p>
-//       </div>
-//     )
-//   }
-  
-//   export default Pill;
-
-  interface PillProps {
-    title: string;
+import { PillProps } from "@/interfaces";
+const Pill: React.FC<PillProps> = ({ title }) => {
+    return (
+      <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
+        <p className=" text-sm ">{title}</p>
+      </div>
+    )
   }
   
-  const Pill: React.FC<PillProps> = ({ title }) => {
-    return (
-      <div className="flex justify-center items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
-        <p className="text-sm">{title}</p>
-      </div>
-    );
-  };
-  
   export default Pill;
-  
