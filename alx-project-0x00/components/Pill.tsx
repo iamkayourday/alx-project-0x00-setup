15cc01767d0b928c94c5c79f36700a7db42f0138
Abdulbasit Imam <abdulbasitkayode@gmail.com>
2024-12-18 02:07:14 +0100

    Set up Card and Pill components, configured Pill to accept title prop

---
+++ alx-project-0x00/components/Pill.tsx
@@ -0,0 +1,24 @@
+// const Pill: React.FC = () => {
+//     return (
+//       <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
+//         <p className=" text-sm ">Title</p>
+//       </div>
+//     )
+//   }
+  
+//   export default Pill;
+
+  interface PillProps {
+    title: string;
+  }
+  
+  const Pill: React.FC<PillProps> = ({ title }) => {
+    return (
+      <div className="flex justify-center items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
+        <p className="text-sm">{title}</p>
+      </div>
+    );
+  };
+  
+  export default Pill;
+  
